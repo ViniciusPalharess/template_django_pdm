1427d9c7bd3bd5d85d55115101999d35ba1aec40
ViniciusPalharess <depaulapalharesv@gmail.com>
2024-04-06 11:50:56 -0300

    Criação da model de Marca

--- core/models/marca.py
+++ core/models/marca.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class MArca(models.Model):
+class Marca(models.Model):
     nome = models.CharField(max_length=50)
     def __str__(self):
         return self.nome.upper()
